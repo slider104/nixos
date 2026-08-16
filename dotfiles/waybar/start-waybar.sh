49954372e5983d70e27d51910f64b2132ae96f54
slider <matze33442@gmx.de>
2026-08-16 18:53:39 +0200

    Update start-waybar.sh

--- dotfiles/waybar/start-waybar.sh
+++ dotfiles/waybar/start-waybar.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-exec /home/slider/.nix-profile/bin/waybar -c /home/slider/nixos/dotfiles/waybar/config -s /home/slider/nixos/dotfiles/waybar/style.css
+exec waybar -c /home/slider/nixos/dotfiles/waybar/config -s /home/slider/nixos/dotfiles/waybar/style.css
