77f11463d30a51aa0fcf35f6f846183d277bd2e2
slider <matze33442@gmx.de>
2026-08-16 09:09:57 +0200

    Update niri.nix

--- modules/niri/niri.nix
+++ modules/niri/niri.nix
@@ -50,7 +50,7 @@ in
     #     "systemd-user-sessions.service"
     #     "sound.target"
       ];
-      ExecStartPre = [
+      preStart = [
         "${pkgs.bash}/bin/bash -c 'until [ -e /dev/dri/renderD128 ]; do sleep 0.1; done'"
       ];
     #   wants = [ "display-manager.service" ];
