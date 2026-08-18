585e68ea460783aeec483af8d0f608aa23ae5fea
slider <matze33442@gmx.de>
2026-08-18 21:56:27 +0200

    Update power-menu.sh

--- dotfiles/waybar/power-menu.sh
+++ dotfiles/waybar/power-menu.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-choice=$(printf "Shutdown\nReboot\nSuspend\nLogout" | fuzzel --dmenu -p "ESC to close" --width=20 --lines=4)
+choice=$(printf "Shutdown\nReboot\nSuspend\nLogout" | fuzzel --dmenu --hide-prompt --mesg="ESC to close" --width=20 --lines=4)
 
 case "$choice" in
   Shutdown)
