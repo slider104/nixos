111cefce2fcbeff6abe3ae0689f6d8bc0951132d
slider <matze33442@gmx.de>
2026-09-06 16:09:04 +0200

    Update virt.nix

--- modules/virt/virt.nix
+++ modules/virt/virt.nix
@@ -22,8 +22,18 @@ in
 
     virtualisation.libvirtd = {
       enable = true;
-      daemonSocket = true;
+      qemu.vhostUserPackages = [ ];
     };
+
+    systemd.services.libvirtd.serviceConfig.ExecStart = lib.mkForce "${pkgs.libvirt}/bin/libvirtd --config /etc/libvirt/libvirtd.conf";
+
+      environment.etc."libvirt/libvirtd.conf".text = ''
+        unix_sock_ro_perms = "0777"
+        unix_sock_rw_perms = "0770"
+        auth_unix_ro = "none"
+        auth_unix_rw = "none"
+        auth_polkit = 0
+      '';
 
     security.polkit.extraConfig = ''
       polkit.addRule(function(action, subject) {
