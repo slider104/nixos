652289b93c8b82bee245d96e6a4a6e9e44ecc903
slider <matze33442@gmx.de>
2026-09-06 16:18:33 +0200

    Update virt.nix

--- modules/virt/virt.nix
+++ modules/virt/virt.nix
@@ -27,14 +27,6 @@ in
 
     systemd.services.libvirtd.serviceConfig.ExecStart = lib.mkForce "${pkgs.libvirt}/bin/libvirtd --config /etc/libvirt/libvirtd.conf";
 
-      environment.etc."libvirt/libvirtd.conf".text = ''
-        unix_sock_ro_perms = "0777"
-        unix_sock_rw_perms = "0770"
-        auth_unix_ro = "none"
-        auth_unix_rw = "none"
-        auth_polkit = 0
-      '';
-
     security.polkit.extraConfig = ''
       polkit.addRule(function(action, subject) {
         if (action.id == "org.libvirt.unix.manage" &&
